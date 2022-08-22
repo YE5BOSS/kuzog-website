9b5bc1c54165723848281a5db6e567fa05b17beb
Cem Gunay <cgunay@uwaterloo.ca>
2022-08-22 16:35:01 -0400

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinksItems, FooterLinkTitle, FooterLink } from './FooterElements'
+import { SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElements'
+import { animateScroll as scroll } from 'react-scroll'
+
+const Footer = () => {
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    }
+
+  return (
+    <>
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinksItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinksItems>
+                        <FooterLinksItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinksItems>
+                        <FooterLinksItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinksItems>
+                        <FooterLinksItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/signin">How it works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinksItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/' onClick={toggleHome}>KUZOG</SocialLogo>
+                        <WebsiteRights>KUZOG © {new Date().getFullYear()} All Rights Reserved</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href='/' target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                            <SocialIconLink href='/' target="_blank" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText : true,
+    lightTextDesc: true,
+    topLine: 'Premium Consulting',
+    headline: 'Advice For General And Limited Partners',
+    description: 'Get access to our exclusive consulting on strategy and tactics for initiating discussions with prospective investors',
+    buttonLabel: 'Get Started',
+    imgStart: true,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Invest',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText : false,
+    lightTextDesc: false,
+    topLine: 'Premium Consulting',
+    headline: 'Advice For General And Limited Partners',
+    description: 'Get access to our exclusive consulting on strategy and tactics for initiating discussions with prospective investors',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Invest',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText : false,
+    lightTextDesc: false,
+    topLine: 'Premium Consulting',
+    headline: 'Advice For General And Limited Partners',
+    description: 'Get access to our exclusive consulting on strategy and tactics for initiating discussions with prospective investors',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Invest',
+    dark: false,
+    primary: false,
+    darkText: true
+}
