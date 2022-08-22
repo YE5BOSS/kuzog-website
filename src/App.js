0ed1773a3420bd4f75603f1b944a174948b8d390
Cem Gunay <cgunay@uwaterloo.ca>
2022-08-22 17:10:49 -0400

    hashrouter fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages'
 import SigninPage from './pages/signin';
 
