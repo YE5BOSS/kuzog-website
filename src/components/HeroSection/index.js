5084071ed50da97eb9d48c77ba3c51c80667c54b
Cem Gunay <cgunay@uwaterloo.ca>
2022-08-22 22:58:25 -0400

    revert

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -20,7 +20,7 @@ const HeroSection = () => {
   return (
     <HeroContainer id='home'>
         <HeroBg>
-            <VideoBg autoPlay loop muted playsInline src={Video} type='video/mp4'/>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
         </HeroBg>
         <HeroContent>
             <HeroH1>Connecting People And Places</HeroH1>
